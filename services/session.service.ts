9dce838dedd798d3898825a5c2480972c5be8bd0
mgalihpp <muhammadgalih451@gmail.com>
2024-12-25 15:29:58 +0700

    Refactor session management: move SessionService to services, update session handling logic, and remove legacy session code

---
+++ services/session.service.ts
@@ -0,0 +1,123 @@
+'use server';
+
+import { Session, SessionPayload, ValidateSession } from '@/types/auth';
+import { jwtVerify, SignJWT } from 'jose';
+import { cookies } from 'next/headers';
+import { db } from '@/lib/prisma';
+import { cache } from 'react';
+import { PrismaClient } from '@prisma/client';
+import PrismaQueryHelper from '@/helpers/prismaQuery';
+
+const secretKey = '2';
+const encodedKey = new TextEncoder().encode(secretKey);
+
+class SessionService {
+  private db: PrismaClient;
+
+  constructor() {
+    this.db = db;
+  }
+
+  async createSession(userId: string) {
+    const expireAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+    const session = await this.encrypt({ userId, expireAt });
+    const cookiesStore = await cookies();
+
+    await this.db.session.create({
+      data: {
+        userId,
+        expireAt,
+      },
+    });
+
+    cookiesStore.set('session', session, {
+      httpOnly: true,
+      secure: true,
+      expires: expireAt,
+    });
+  }
+
+  async deleteSession() {
+    const cookiesStore = await cookies();
+    cookiesStore.delete('session');
+  }
+
+  async encrypt(payload: SessionPayload) {
+    return new SignJWT(payload)
+      .setProtectedHeader({ alg: 'HS256' })
+      .setIssuedAt()
+      .setExpirationTime('7d')
+      .sign(encodedKey);
+  }
+
+  async decrypt(session: string | undefined = '') {
+    try {
+      const { payload } = await jwtVerify(session, encodedKey, {
+        algorithms: ['HS256'],
+      });
+      return payload as Session;
+    } catch (error) {
+      console.log('Failed to verify session ', error);
+    }
+  }
+
+  async getUser(userId: string) {
+    try {
+      const prismaQueryHelper = new PrismaQueryHelper();
+
+      const user = await this.db.user.findFirst({
+        where: {
+          id: userId,
+        },
+        ...prismaQueryHelper.getDefaultUser(),
+      });
+
+      return user;
+    } catch (error) {
+      console.error('Failed to get user ', error);
+      return null;
+    }
+  }
+}
+
+export const getSession = cache(async (): Promise<ValidateSession> => {
+  try {
+    const session = new SessionService();
+    const cookiesStore = await cookies();
+
+    const sessionCookie = cookiesStore.get('session')?.value;
+
+    const sessionData = await session.decrypt(sessionCookie);
+
+    if (!sessionData) {
+      return {
+        user: undefined,
+        session: undefined,
+      };
+    }
+
+    const user = await session.getUser(sessionData.userId);
+
+    if (!user) {
+      return {
+        user: undefined,
+        session: undefined,
+      };
+    }
+
+    const result = {
+      user,
+      session: sessionData,
+    };
+
+    return result;
+  } catch (error) {
+    console.error('Failed to get session ', error);
+    return {
+      user: undefined,
+      session: undefined,
+    };
+  }
+});
+
+export default SessionService;
--- types/auth.ts
+++ types/auth.ts
@@ -1,3 +1,6 @@
+import { User } from '@prisma/client';
+import { JWTPayload } from 'jose';
+
 export type SessionPayload = {
   userId: string;
   expireAt: Date;
@@ -16,3 +19,12 @@ export type LoginAction = (
 export type SignUpAction = (
   params: Credentials
 ) => Promise<ServerActionResponse>;
+
+export type Session = JWTPayload & {
+  userId: string;
+};
+
+export type ValidateSession = {
+  user?: Omit<User, 'passwordHash' | 'googleId'>;
+  session?: Session;
+};
