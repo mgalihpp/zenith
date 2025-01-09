330c475b9d9c4fa9f9ad85b11298f67f12ea51cd
Mmeane <149094186+Mmeane@users.noreply.github.com>
2025-01-09 09:34:57 +0700

    Add Following Tab Content (#14)
    * Add Following Tab Content

---
+++ components/Feed/FollowingFeed.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { api } from '@/lib/api';
+import { PostsPage } from '@/types/post';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import PostsLoadingSkeleton from '@/components/Post/PostsLoadingSkeleton';
+import InfiniteScrollWrapper from '@/components/InfiniteScrollWrapper';
+import Post from '@/components/Post/Post';
+import { Loader2 } from 'lucide-react';
+
+export default function FollowingFeed() {
+  const {
+    data,
+    status,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: [''],
+    queryFn: ({ pageParam }) =>
+      api.get<PostsPage>(
+        `/api/posts/following`,
+        pageParam
+          ? {
+              searchParams: {
+                cursor: pageParam,
+              },
+            }
+          : {}
+      ),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.data.nextCursor,
+  });
+
+  const posts = data?.pages.flatMap((page) => page.data?.posts) ?? [];
+
+  if (status === 'pending') {
+    return <PostsLoadingSkeleton />;
+  }
+
+  if (status === 'success' && !posts && !hasNextPage) {
+    return (
+      <p className="text-center text-muted-foreground">
+        No one has posted anything yet.
+      </p>
+    );
+  }
+
+  if (status === 'error') {
+    return (
+      <p className="text-center text-destructive">
+        An error occurred while loading posts.
+      </p>
+    );
+  }
+
+  return (
+    <InfiniteScrollWrapper
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+    </InfiniteScrollWrapper>
+  );
+}
