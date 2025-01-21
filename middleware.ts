9b53bd5eb574df01ab0fbfacff135cdb5161f292
mgalihpp <muhammadgalih451@gmail.com>
2025-01-21 17:26:43 +0700

    refactor middleware by commenting out unused code

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,26 @@
-import { NextRequest, NextResponse } from 'next/server';
-import SessionService from './services/session.service';
+// import { NextRequest, NextResponse } from 'next/server';
+// import SessionService from './services/session.service';
 
-export function middleware(req: NextRequest) {
-  const cookies = req.cookies;
-  const sessionCookie = cookies.get('session')?.value;
-  const sessionService = new SessionService();
+// export function middleware(req: NextRequest) {
+//   const cookies = req.cookies;
+//   const sessionCookie = cookies.get('session')?.value;
+//   const sessionService = new SessionService();
 
-  if (!req.cookies.has('session')) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
+//   if (!req.cookies.has('session')) {
+//     return NextResponse.redirect(new URL('/login', req.url));
+//   }
 
-  const sessionData = sessionService.decrypt(sessionCookie);
+//   const sessionData = sessionService.decrypt(sessionCookie);
 
-  if (!sessionData) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
+//   if (!sessionData) {
+//     return NextResponse.redirect(new URL('/login', req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: [
-    '/((?!api|session|login|signup|_next/static|_next/image|favicon.ico).*)', // Exclude /api/*, /login, and /signup routes
-  ],
-};
+// export const config = {
+//   matcher: [
+//     '/((?!api|session|login|signup|_next/static|_next/image|favicon.ico).*)', // Exclude /api/*, /login, and /signup routes
+//   ],
+// };
