64bebb69d6e370a7c781733f8f1e106d083fff66
mgalihpp <muhammadgalih451@gmail.com>
2025-01-21 17:41:31 +0700

    refactor: update callback URL and comment out unused API import

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,12 +1,12 @@
 import { createRouteHandler } from 'uploadthing/next';
 import { fileRouter } from './core';
-import { api } from '@/lib/api';
+// import { api } from '@/lib/api';
 
 export const { GET, POST } = createRouteHandler({
   router: fileRouter,
   config: {
     logLevel: 'Debug',
-    callbackUrl: api.getBaseUrl() + '/api/uploadthing',
+    callbackUrl: 'https://zenith-six-rho.vercel.app/api/uploadthing',
     isDev: process.env.NODE_ENV === 'development',
   },
 });
