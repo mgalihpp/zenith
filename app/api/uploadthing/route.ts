f01d6baa2411a6492ac898d00ce47e77b9ab74b2
mgalihpp <muhammadgalih451@gmail.com>
2025-01-21 16:57:54 +0700

    refactor set to dev test

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -5,8 +5,8 @@ import { api } from '@/lib/api';
 export const { GET, POST } = createRouteHandler({
   router: fileRouter,
   config: {
-    // logLevel: 'Debug',
+    logLevel: 'Debug',
     callbackUrl: api.getBaseUrl() + '/api/uploadthing',
-    isDev: process.env.NODE_ENV === 'development',
+    isDev: true,
   },
 });
