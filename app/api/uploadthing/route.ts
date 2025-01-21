a95a68abfbd0e9d2b61b28e82df8428cc73490cb
mgalihpp <muhammadgalih451@gmail.com>
2025-01-21 16:19:21 +0700

    refactor callback url

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,12 +1,15 @@
 import { createRouteHandler } from 'uploadthing/next';
 import { fileRouter } from './core';
-import { api } from '@/lib/api';
+// import { api } from '@/lib/api';
 
 export const { GET, POST } = createRouteHandler({
   router: fileRouter,
   config: {
     // logLevel: 'Debug',
-    callbackUrl: api.getBaseUrl() + '/api/uploadthing',
+    callbackUrl:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000'
+        : 'https://zenith-six-rho.vercel.app/' + '/api/uploadthing',
     isDev: process.env.NODE_ENV === 'development',
   },
 });
