23cdcfc1ca79001d55599cb80dac867b00821288
mgalihpp <muhammadgalih451@gmail.com>
2025-01-21 16:45:50 +0700

    Refactor callback URL

--- lib/api.ts
+++ lib/api.ts
@@ -61,7 +61,7 @@ class ApiRequest {
 
   getBaseUrl() {
     if (typeof window !== 'undefined') return window.location.origin;
-    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+    if (process.env.VERCEL_URL) return `${process.env.VERCEL_URL}`;
     return `http://localhost:${process.env.PORT ?? 3000}`;
   }
 }
